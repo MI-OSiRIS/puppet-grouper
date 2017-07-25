9eab7702134871730348a7a3501c565fd0f35e62
Ben Meekhof <bmeekhof@users.noreply.github.com>
2017-07-25 10:30:48 -0400

    First version of module

---
+++ manifests/init.pp
@@ -0,0 +1,160 @@
+# This module will manage grouper configurations but does not do everything that the grouperInstaller does.
+# You still will need to manually download and extract + patch the grouper archives.
+# You can setup grouperInstaller to do this non-interactively:
+# https://spaces.internet2.edu/display/Grouper/Grouper+installer+non+interactive+auto+apply+patches+example
+#
+# This module does not initialize the grouper database.
+# Sample schema are here:  https://spaces.internet2.edu/display/Grouper/Grouper+Installer
+
+# You can optionally run the grouperInstaller to take care of download, patching, database and then use this module to
+# finish the configuration and manage the installation on an ongoing basis.
+# It may also be useful to copy your installation and manage different test configs with additional grouper::instance resources.
+
+# Configs defined as grouper::instance so multiple grouper instances can be managed on same host
+#
+# configures:
+#  - subject API adapter in sources.xml  (only a single LDAP adapter is supported)
+#  - grouper.properties wheel group autocreate config
+#  - log4j config to choose logfile location
+#  - grouper database configuration (does not initialize database)
+#
+# Taken from steps at https://spaces.internet2.edu/display/COmanage/Grouper+Provisioning+Plugin
+# Also from https://spaces.internet2.edu/download/attachments/93651000/TI.25.1-TIERGrouperDeploymentGuide.pdf
+#
+# If you choose to follow instructions from Comanage plugin installation to replace jdbc clients
+# then set detect_utf8_problems and log_driver_mismatch false.
+#
+# It is probably not strictly necessary, the TIER deployment guide does not have that step and is more recent
+# My test installation was fine with the packaged database drivers but I did later update
+# Jarfile: https://downloads.mariadb.com/Connectors/java/connector-java-2.0.3/mariadb-java-client-2.0.3.jar
+#
+# also requires margascon::profile_d
+#
+# Class Params:
+#
+# manage_groovy
+# 	install groovy package (does not setup groovysh4grouper: https://github.com/wgthom/groovysh4grouper)
+#
+# groovy_install
+#		'download' will grab binary archive from dl.bintray.com.  Requires example42-puppi module.
+#		'package' will install with dist package from pre-existing repo
+#
+# groovy_version
+#	which version to download if groovy_install is set to 'download'
+#
+# manage_tomcat
+#	install and configure tomcat.  Requires puppetlabs-tomcat module.  RHEL7 only.  Build .war file from the installation(s) managed
+#   in grouper::instance resources and place in /usr/share/tomcat/webapps.
+#
+# grouper_system_password
+#    if manage_tomcat is true configure grouper_system role and GrouperSystem user in tomcat-users.xml
+
+class grouper(
+	$manage_groovy = true,
+	$manage_tomcat = true,
+	$groovy_version = '2.4.12',
+	$groovy_install = 'download',
+	$grouper_system_password = undef
+) {
+
+	if $manage_tomcat {
+
+		class { 'tomcat':
+			catalina_home       => '/usr/share/tomcat'
+		}
+
+		# Just using a few basic resources from mdule
+		tomcat::install { 'grouper_tomcat':
+			install_from_source => false,
+			package_name        => 'tomcat',
+		}
+
+		tomcat::config::server::connector { 'grouper-http':
+			protocol              => 'HTTP/1.1',
+			port                  => '8080',
+			additional_attributes => {
+				'URIEncoding' => 'UTF-8'
+			}
+		}
+
+		tomcat::config::server::connector { 'grouper-ajp':
+			protocol              => 'AJP/1.3',
+			port                  => '8009',
+			additional_attributes => {
+				'URIEncoding' => 'UTF-8'
+			}
+		}
+
+		# tomcat::service could be used here but would need non-default params (and also defaults to using service instead of systemctl)
+		service { 'tomcat':
+			ensure => running,
+			enable => true,
+		}
+
+		file_line { 'sysconfig_tomcat':
+			path   => '/etc/sysconfig/tomcat',
+			line   => 'JAVA_OPTS="-server -Xmx512M -XX:MaxPermSize=256M"',
+			match  => '^JAVA_OPTS\=',
+			notify => Service['tomcat']
+		}
+
+		# we are probably going to need a way to manage grouper-ws users in this file (if this is how we auth the api)
+		# https://spaces.internet2.edu/display/Grouper/Grouper+WS+Authentication
+		# for the time being GrouperSystem can be used, maybe that will be sufficient for our needs
+
+		if $password {
+			tomcat::config::server::tomcat_users { 'grouper_system':
+				element => 'role'
+			}
+
+			tomcat::config::server::tomcat_users { 'GrouperSystem':
+				element  => 'user',
+				password => $password,
+				roles    => [ 'grouper_user' ]
+			}
+		}
+
+	}
+
+	package {
+		[
+			'java-1.8.0-openjdk',
+			'java-1.8.0-openjdk-devel',
+			'mariadb-server',
+		]: ensure => present
+	}
+
+	if $manage_groovy {
+		if $groovy_install == 'download' {
+		    puppi::netinstall { 'groovysh':
+		        url                 => "https://dl.bintray.com/groovy/maven/apache-groovy-binary-${groovy_version}.zip",
+		        work_dir            => '/tmp',
+		        extracted_dir       => "groovy-${groovy_version}",
+		        destination_dir     => '/opt',
+		        postextract_command => "ln -s /opt/groovy-${groovy_version} /opt/groovy ; ln -s /opt/groovy/bin/groovysh /usr/bin/groovysh"
+		    }
+		} else {
+			package { 'groovy': ensure => present }
+		}
+	}
+
+	# this should be part of a managed mariadb instance which also does these things:
+	# create database grouper;
+	# create user 'grouper'@'localhost' identified by 'somepass';
+	# grant all privileges on grouper.* to 'grouper'@'localhost';
+
+	# do not use utf8mb4, requires changes to database column definitions because of character limit of 191 in utf8mb4 (more bits per character means less max characters)
+
+	# leave to user to restart
+	file  { '/etc/my.cnf.d/charset.cnf':
+		content =>  "[mysqld] \ncharacter-set-server = utf8 \ncollation-server = utf8_bin"
+	}
+
+}
+
+
+
+
+
+
+
---
+++ manifests/instance.pp
@@ -0,0 +1,210 @@
+#
+# Resource to define instance of grouper.
+# Must first be installed by some means such as grouperInstaller
+# or by unpacking tarballs and initializing DB manually
+#
+#  Params:
+#
+#  grouper_topdir
+#       Top level directory where grouper.apiBinary-<version>, grouper.ui-<version>, etc live.
+#        GROUPER_HOME and various paths will be constructed from topdir and version
+#
+#    grouper_version
+#        Version installed under grouper_topdir, used in constructing GROUPER_HOME.
+#        Default 2.3.0 (current release at this time)
+#
+#    ui_host
+#        http(s)://hostname:port for grouper web ui and web API (path specified separately).
+#        Default fqdn:8080
+#
+#    ui_path
+#        Path component of URL for grouper web ui without / prefix.  Default 'grouper'.
+#
+#    ws_path
+#        Path component of URL for grouper web services API without / prefix.  Default 'grouper-ws'.
+#
+#    tomcat_context
+#        Values:  present, absent.  Default present.
+#        Create tomcat server context referencing ws/ui paths in grouper_topdir.
+#        Only relevant if grouper::manage_tomcat was set true.  You may choose not to set this
+#        and instead build .war files for ws and ui to install in your tomcat webapps directory.
+#        See https://spaces.internet2.edu/display/Grouper/Grouper+UI+Installation
+#
+#    environment_name
+#        Sets grouper.env.name to distinguish instances.  Defaults to name of resource.
+#
+#    grouper_user
+#        What user tomcat runs as.  Also sets permissions on logdir.  Default 'tomcat'.
+#
+#    grouper_group
+#        Group that tomcat runs as.  Also sets permissions on logdir. Default 'tomcat'.
+#
+#    logdir
+#        Where grouper components log.  Sets appropriate setting in log4j.properties for each component.
+#
+#    db_host
+#        Host to connect to for database access.  Default 'localhost'.
+#
+#    db_name
+#        Name of grouper database.  Default 'grouper'.
+#
+#    db_type
+#        Type of database.  Determines driver settings.  Currently 'mariadb' and 'mysql' supported.
+#        Default 'mariadb'.  You will need to install mariadb jarfile, steps 4-8 here (other config steps are in this module):
+#        https://spaces.internet2.edu/display/COmanage/Grouper+Provisioning+Plugin%3A+Post+Grouper+Install+Configuration
+#        (TODO:  Module should install it)
+#
+#    db_user
+#        Username to authenticate to database.
+#
+#    db_password
+#        Password to authenticate to database.
+#
+#    detect_utf8_problems
+#        Set configuration.detect.utf8.file.problems in grouper.properties.
+#        Should generally be false.
+#
+#    log_driver_mismatch
+#        Should be false if using mariadb driver.  Default false.
+#
+#    adapter_config
+#        A hash defining ldap subject id source adapter configuration.  Only 'ldap' type is supported.
+#        More information on the meaning of these configuration items is available from the Grouper wiki:
+#        https://spaces.internet2.edu/display/Grouper/Subject+API
+#        https://spaces.internet2.edu/display/Grouper/LDAP+Subject+API+example
+#
+#        Example:
+#
+#        $adapter_config = {
+#            'adapter_type'     => 'ldap',
+#            'adapter_id'       => 'name',
+#            'adapter_name'     => 'name',
+#            'provider_url'     => 'ldaps://ldap.example.org:636/dc=example,dc=org',
+#            'subject_id_type'  => 'eduPersonPrincipalName',
+#            'subject_ou'       => 'People',
+#            'name_type'        => 'cn',
+#            'description_type' => 'gecos'
+#        }
+#
+#    default
+#        If this is true then a GROUPER_HOME environment variable will be set in /etc/profile.d/grouper-${name}.sh
+
+define grouper::instance (
+    $grouper_topdir        = '/opt/grouper',
+    $grouper_version       = '2.3.0',
+    $ui_host               = '${fqdn}:8080',
+    $ws_path               = 'grouper-ws',
+    $ui_path               = 'grouper',
+    $tomcat_context        = 'present',
+    $environment_name      = "${name}",
+    $grouper_user          = 'tomcat',
+    $grouper_group         = 'tomcat',
+    $logdir                = '/var/log/grouper',
+    $db_host               = 'localhost',
+    $db_name               = 'grouper',
+    $db_type               = 'mariadb',
+    $db_user               = 'grouper',
+    $db_password           = undef,
+    $detect_utf8_problems  = false,
+    $log_driver_mismatch   = false,
+    $adapter_config        = undef,
+    $default               = true
+) {
+
+    $grouper_home = "${grouper_topdir}/grouper.apiBinary-${grouper_version}"
+    $ui_url = "${ui_host}/${ui_path}"
+
+    exec { "grouper-installed-${name}":
+            command => '/bin/false',
+            unless => "/bin/test -e $grouper_home"
+    }
+
+    file { "$logdir":
+        ensure => directory,
+        owner  => $grouper_user,
+        group  => $grouper_group
+    }
+
+    if $::grouper::manage_tomcat {
+        tomcat::config::server::context { "grouper-ws-context-${name}":
+            context_ensure => $tomcat_context,
+            doc_base => "${grouper_topdir}/grouper.ws-${grouper_version}/grouper-ws/build/dist/grouper-ws",
+            additional_attributes => {
+                'path' => "/${ws_path}",
+                'reloadable' => 'false',
+                'mapperContextRootRedirectEnabled' => 'true',
+                'mapperDirectoryRedirectEnabled' => 'true'
+            }
+        }
+        tomcat::config::server::context { "grouper-ui-context-${name}":
+            context_ensure => $tomcat_context,
+            doc_base => "${grouper_topdir}/grouper.ui-${grouper_version}/dist/grouper",
+            additional_attributes => {
+                'path' => "/${ui_path}",
+                'reloadable' => 'false',
+                'mapperContextRootRedirectEnabled' => 'true',
+                'mapperDirectoryRedirectEnabled' => 'true'
+            }
+        }
+
+    }
+
+    case $db_type {
+        'mariadb','mysql': {
+            $hibernate_connection_url = "jdbc:mysql://${db_host}:3306/${db_name}?autoReconnect=true&CharSet=utf8&useUnicode=true&characterEncoding=utf8"
+            $hibernate_driver_dialect  = 'org.hibernate.dialect.MySQL5Dialect'
+            if $db_type == 'mariadb' {
+                $hibernate_driver_class = 'org.mariadb.jdbc.Driver'
+            } else {
+                $hibernate_driver_class = 'org.mysql.jdbc.Driver'
+            }
+        }
+    }
+
+    $grouper_properties = [
+        "grouper.apiBinary-${grouper_version}/conf",
+        "grouper.ui-${grouper_version}/dist/grouper/WEB-INF/classes",
+        "grouper.ws-${grouper_version}/grouper-ws-scim/targetBuiltin/grouper-ws-scim/WEB-INF/classes",
+        "grouper.ws-${grouper_version}/grouper-ws/build/dist/grouper-ws/WEB-INF/classes"
+    ]
+
+     $grouper_propfiles_c = [ 'grouper.properties','log4j.properties', 'grouper.hibernate.properties' ]
+
+    if $adapter_config {
+        $grouper_propfiles = concat($grouper_propfiles_c, 'sources.xml')
+    } else {
+        $grouper_propfiles = $grouper_propfiles_c
+    }
+
+    $grouper_properties.each | $propdir | {
+        $grouper_propfiles.each | $propfile | {
+            file { "${grouper_topdir}/${propdir}/${propfile}":
+                content => template("grouper/${propfile}.erb"),
+                owner => $grouper_user,
+                group => $grouper_group,
+                require => Exec["grouper-installed-${name}"]
+            }
+        }
+
+        # There were too many attributes to insert for this to be a very appealing approach
+        # it might be nice to preserve the distributed sources.xml - the file we copy in place
+        # was copied from the original in full and has our ldap source inserted
+        # augeas{ "${name}-adapter-config-${propdir}":
+                #   incl => "${grouper_topdir}/${propdir}/sources.xml",
+                #   lens => "Xml.lns",
+                #   context => "/files/${grouper_topdir}/${propdir}/sources.xml/sources",
+                #   changes => []
+        # }
+
+    }
+
+    if $default {
+        profile_d::script { "grouper-${name}.sh":
+            content => "export GROUPER_HOME=$grouper_home"
+        }
+    }
+#
+
+
+}
+
